b6048063416618819b320b11edb1963034f2096d
AaronTheNerd <bargeaaron@gmail.com>
2020-10-07 16:46:27 -0400

    Done

---
+++ case-study/HW01TestDirectory/mydirectory/bargeaaron_hwEC/Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/****************************************************************
+ * Main program for Extra Credit.
+ *
+ * Author/copyright:  Duncan Buell. All rights reserved.
+ * Date: 28 December 2014
+ *
+ * Revised: Aaron Barge.
+ * Date: 7 October 2020.
+**/
+
+int main(int argc, char *argv[])
+{
+  cout << "Hello, world." << endl;
+  cout << "My name is Aaron Barge." << endl;
+
+  return 0;
+}
+
